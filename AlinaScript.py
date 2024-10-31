a098f901380c0fb6deaf2b26b7f5aedd6cfab894
Alinaa1124 <a46545978@gmail.com>
2024-10-31 17:18:54 +0200

    Update AlinaScript.py

--- AlinaScript.py
+++ AlinaScript.py
@@ -1,20 +1,16 @@
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-def find_primes(n):
-    primes = []
-    for i in range(2, n + 1):
-        if is_prime(i):
-            primes.append(i)
-    return primes
+def sieve_of_eratosthenes(n):
+    if n < 2:
+        return []
+    primes = [True] * (n + 1)
+    primes[0], primes[1] = False, False  # 0 и 1 не являются простыми числами
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+    return [i for i in range(n + 1) if primes[i]]
 
 # Ввод от пользователя
 n = int(input("Введите предел для поиска простых чисел: "))
 
 # Нахождение и вывод простых чисел
-print("Простые числа:", find_primes(n))
+print("Простые числа:", sieve_of_eratosthenes(n))
